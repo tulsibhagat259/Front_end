afc011dede63f82fd8476d61ab144d079236c85a
tulsibhagat259 <riya.bhagat.259@gmail.com>
2025-11-26 19:19:29 +0530

    css

---
+++ javascript/ex.js
@@ -0,0 +1,10 @@
+// console.log("Hello this External Js");
+// console.log("Hello");
+// console.log("mul ->",10*7)
+
+// Bom :- borswer object model
+
+// alert("pls filed first data")
+// confirm("Are your sure ?")
+// prompt("5 + 5 => ?")
+// print()
